3f0e15ba75b7e9d708c23d171367d4bf2d5c043d
Kelly Park <sehyun.kelly.park@gmail.com>
2023-01-08 03:37:31 -0800

    UploadServer modified and ServletRequest added to parse the request

---
+++ ServletRequest.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+public class ServletRequest
+{
+    private string reqeust;
+    private string[] lines;
+
+    public ServletRequest(string req){
+        this.reqeust = req;
+        lines = req.Split("\n");
+
+        // for(int i = 0; i < lines.Length; i++){
+        //     Console.WriteLine("Line Number: " + i + lines[i]);
+        // }
+    }
+    
+    public string getFileName(){
+        String fileName = "";
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i].Contains("filename"))
+            {
+                string[] names = lines[i].Split("filename=\"");
+                fileName = names[1].Substring(0, names[1].Length - 2);
+                break;
+            }
+        }
+
+        return fileName;
+    }
+    public string getCaption(){
+        String cap = "";
+        
+        for(int i = 0; i < lines.Length; i++){
+            if(lines[i].Contains("name=\"caption\"")){
+                cap = lines[i + 2];
+                break;
+            }
+        }
+
+        return cap;
+    }
+
+    public string getDate()
+    {
+        String date = "";
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i].Contains("name=\"date\""))
+            {
+                date = lines[i + 2];
+                break;
+            }
+        }
+
+        return date;
+    }
+}
--- UploadServer.cs
+++ UploadServer.cs
@@ -1,71 +1,112 @@
-using System; 
-using System.IO; 
-using System.Collections.Generic; 
-using System.Linq; 
-using System.Text; 
-using System.Threading.Tasks; 
-using System.Threading; 
-using System.Net; 
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
 using System.Net.Sockets;
+using static ServletRequest;
 
-public class DirListing {
+public class DirListing
+{
 
-        Socket cls = null;
+    Socket cls = null;
 
-        public DirListing(Socket socket) { this.cls = socket; }
+    public DirListing(Socket socket) { this.cls = socket; }
 
-        public void threadMethod() {
-                    Byte[] bytesReceived = new Byte[1];
-                    string req = "";
-                    bool stop = false;
-                    IAsyncResult result;
-                    Action action = () =>
-                    {
-                        while (!stop) {
-                            // Commented out to test if the second clause after || prevents the Post req from printing 
-                            // if ((cls.Receive(bytesReceived, bytesReceived.Length, 0) == 0) ||
-                            // (Encoding.ASCII.GetString(bytesReceived, 0, 1)[0] == '\0')) {
-                            //     break;
-                            // }
-                            if ((cls.Receive(bytesReceived, bytesReceived.Length, 0) == 0)) {
-                                break;
-                            }
-                            req += Encoding.ASCII.GetString(bytesReceived, 0, 1);
-                        }
-                    };
-                    result = action.BeginInvoke(null, null);
-                    if (!result.AsyncWaitHandle.WaitOne(1000)) stop = true;
+    public void threadMethod()
+    {
+        Byte[] bytesReceived = new Byte[1];
+        string req = "";
+        bool stop = false;
+        IAsyncResult result;
 
-                    Console.WriteLine(req);
-                    string res;
-                    if(req.StartsWith("G")){
-                        res = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: 513\r\n\r\n<!DOCTYPE html>\r\n<html>\n   <head>\n       <title>File Upload Form</title>\n   </head>\n   <body>\n       <h1>Upload file</h1>\n       <form method=\"POST\" action=\"upload\" enctype=\"multipart/form-data\">\n           <input type=\"file\" name=\"fileName\"/><br/><br/>\n           Caption: <input type=\"text\" name=\"caption\"<br/><br/><br/>\n           Date: <input type=\"date\" name=\"date\"<br/><br/><br/>\n           <input type=\"submit\" value=\"Submit\"/>\n       </form>\n   </body>\n</html>\r\n\r\n";
-                        cls.Send(System.Text.Encoding.ASCII.GetBytes(res + '\0'), 0);
-                    }else{
-                        res = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\r\n<html>\n   <head>\n       <title>File Upload</title>\n   </head>\n   <body>\n       <h1>This is after POST</h1>\n </body>\n</html>\r\n\r\n";
-                        cls.Send(System.Text.Encoding.ASCII.GetBytes(res + '\0'), 0);
-                    }
-                    cls.Close();
-        }
+        Action action = () =>
+        {
+            // Console.WriteLine("Async thread");
+            while (!stop)
+            {
+                cls.Receive(bytesReceived, bytesReceived.Length, 0);
+                req += Encoding.ASCII.GetString(bytesReceived, 0, 1);
 
-        static void Main(string[] args) {
-            try {
-                int port = 8888;  
-                IPAddress address = IPAddress.Parse("127.0.0.1");
-                IPEndPoint ipe = new IPEndPoint(address, port);
-                Socket s = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                s.Bind(ipe); s.Listen(10);
-                while (true) {
-                    Socket cls = s.Accept();
-                    DirListing dirListing = new DirListing(cls);
-                    Thread thread = new Thread(new ThreadStart(dirListing.threadMethod));
-                        thread.Start();
+                if(req.StartsWith("G") && req.Contains("Accept-Language"))
+                {
+                    Console.WriteLine("GET request");
+                    break;
                 }
-                s.Close();
+
+                if(req.StartsWith("P") && countBoundary("------WebKitFormBoundary", req) >= 4)
+                {
+                    Console.WriteLine("POST request");
+                    break;
+                }
             }
-            catch (SocketException e) {
-                Console.WriteLine("Socket exception: {0}", e);
-            }
+        };
+
+
+        result = action.BeginInvoke(null, null);
+        // Console.WriteLine("Main thread");
+        // if (!result.AsyncWaitHandle.WaitOne(1000)) stop = true;
+        result.AsyncWaitHandle.WaitOne();
+
+        ServletRequest servletRequest = new ServletRequest(req);
+        Console.WriteLine(req);
+        Console.WriteLine("Length: " + req.Length);
+        string res;
+        if (req.StartsWith("G"))
+        {
+            res = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: 513\r\n\r\n<!DOCTYPE html>\r\n<html>\n   <head>\n       <title>File Upload Form</title>\n   </head>\n   <body>\n       <h1>Upload file</h1>\n       <form method=\"POST\" action=\"upload\" enctype=\"multipart/form-data\">\n           <input type=\"file\" name=\"fileName\"/><br/><br/>\n           Caption: <input type=\"text\" name=\"caption\"<br/><br/><br/>\n           Date: <input type=\"date\" name=\"date\"<br/><br/><br/>\n           <input type=\"submit\" value=\"Submit\"/>\n       </form>\n   </body>\n</html>\r\n\r\n";
+            Byte[] msg = System.Text.Encoding.ASCII.GetBytes(res + '\0');
+            cls.Send(msg, msg.Length, 0);
         }
+        else if (req.StartsWith("P"))
+        {
+            res = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\r\n<html>\n   <head>\n       <title>File Upload</title>\n   </head>\n   <body>\n       ";
+            res += "<h1>Files uploaded:</h1>\n";
+            res += "<p> File Name:" + servletRequest.getFileName() + ", Caption: " + servletRequest.getCaption() + ", Date: " + servletRequest.getDate() + "</p>\n";
+            res += "</body>\n</html>\r\n\r\n";
+            Byte[] msg = System.Text.Encoding.ASCII.GetBytes(res + '\0');
+            cls.Send(msg, msg.Length, 0);
+        }
+        cls.Close();
+    }
+
+    public int countBoundary(string boundary, string req)
+    {
+        string[] lines = req.Split("\n");
+        int count = 0;
+
+        for(int i = 0; i < lines.Length; i++){
+            if(lines[i].Contains(boundary)) count++;
+        }
+
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            int port = 8888;
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipe = new IPEndPoint(address, port);
+            Socket s = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(ipe); s.Listen(10);
+            while (true)
+            {
+                Socket cls = s.Accept();
+                DirListing dirListing = new DirListing(cls);
+                Thread thread = new Thread(new ThreadStart(dirListing.threadMethod));
+                thread.Start();
+            }
+            s.Close();
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Socket exception: {0}", e);
+        }
+    }
 }
 
